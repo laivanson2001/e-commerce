de4d394153ad6db84aeb77f71159119ab0bb696a
Son Lai <laivanson.pc@gmail.com>
2022-08-01 21:24:15 +0700

    upload

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://i.pinimg.com/564x/59/a4/dc/59a4dcd5f00a747f0166a6888353205e.jpg',
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/564x/62/c7/f9/62c7f96f3055286c1116a440d77b9947.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/564x/43/15/d0/4315d0011be6359dfae103c747b72415.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/564x/8a/fc/42/8afc423a43a431c38ab77c0d6794d997.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/564x/19/64/26/196426063ac0f99158d64b9d93c40e3d.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/564x/64/9e/96/649e96d02d0d01e17bc45ae943ac2a15.jpg",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/564x/cf/ef/66/cfef6645fe6587f9e40f16b77227e125.jpg",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/564x/9d/f5/7c/9df57cf501eb21e58d6a0fd665d22d24.jpg",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/564x/e7/19/26/e719266e68b272b048ee26eba423e8ed.jpg",
+    },
+    {
+        id: 4,
+        img: "https://i.pinimg.com/564x/b9/ac/2a/b9ac2a96b269fec59ee13bde79b4a458.jpg",
+    },
+    {
+        id: 5,
+        img: "https://i.pinimg.com/564x/a3/cc/d0/a3ccd0e744132a4b94ca0dc2b77cd7b4.jpg",
+    },
+    {
+        id: 6,
+        img: "https://i.pinimg.com/564x/b2/43/b0/b243b02fffdd0bc14aeba924f1b7a2d9.jpg",
+    },
+    {
+        id: 7,
+        img: "https://i.pinimg.com/564x/32/4f/0a/324f0a77bcf2ba3f25b3f263e57bf8c7.jpg",
+    },
+    {
+        id: 8,
+        img: "https://i.pinimg.com/564x/f1/8c/3b/f18c3b4ac46a3a32292c0367845e537d.jpg",
+    },
+]
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/NewsLetter'
+import Products from '../components/Products'
+
+const Container = styled.div``
+
+const Title = styled.h1`
+    margin: 20px;
+`
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const Filter = styled.div`
+    margin: 20px;
+`
+
+const FilterText = styled.span`
+    font-size: 26px;
+    font-weight: 600;
+    margin-right: 20px;
+`
+
+const Select = styled.select`
+    padding: 10px;
+    margin-right: 20px;
+`
+
+const Option = styled.option``
+
+const ProductList = () => {
+    return (
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Title>Dress</Title>
+            <FilterContainer>
+                <Filter>
+                    <FilterText>Filter Product: </FilterText>
+                    <Select defaultValue={'none'}>
+                        <Option disabled value={'none'}>
+                            Color
+                        </Option>
+                        <Option value={'white'}>White</Option>
+                        <Option value={'black'}>Black</Option>
+                        <Option value={'red'}>Red</Option>
+                        <Option value={'blue'}>Blue</Option>
+                        <Option value={'yellow'}>Yellow</Option>
+                        <Option value={'green'}>Green</Option>
+                    </Select>
+                    <Select defaultValue={'none'}>
+                        <Option disabled value={'none'}>
+                            Size
+                        </Option>
+                        <Option value={'xs'}>XS</Option>
+                        <Option value={'s'}>S</Option>
+                        <Option value={'m'}>M</Option>
+                        <Option value={'l'}>L</Option>
+                        <Option value={'xl'}>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                    <FilterText>Sort Product: </FilterText>
+                    <Select defaultValue={'newest'}>
+                        <Option value={'newest'}>Newest</Option>
+                        <Option value={'asc'}>Price (asc)</Option>
+                        <Option value={'desc'}>Price (desc)</Option>
+                    </Select>
+                </Filter>
+            </FilterContainer>
+            <Products />
+            <Newsletter />
+            <Footer />
+        </Container>
+    )
+}
+
+export default ProductList
